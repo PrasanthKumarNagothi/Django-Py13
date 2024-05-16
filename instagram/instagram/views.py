dae6440aee0fb934b87f1b83fed29f5f45e0b789
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-05-16 12:11:16 +0530

    Django Templates

--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse('Welcome to Backend with django')
+    # return HttpResponse('Welcome to Backend with django')
+    return render(request, 'home.html')
