01286b5b05b5a6898e3ab2ffc580cd9c8a566f00
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-05-17 12:14:05 +0530

    DTL Dynamic data & Condtional rendering

--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -4,4 +4,25 @@ from django.shortcuts import render
 
 def homepage(request):
     # return HttpResponse('Welcome to Backend with django')
-    return render(request, 'home.html')
+    students = [
+        {
+            "name": "Prakash",
+            "age": 22,
+            "number": 9876554321,
+            "marks": 35
+        },
+        {
+            "name": "Sarath",
+            "age": 23,
+            "number": 87483378437,
+            "marks": 34
+        },
+        {
+            "name": "Yashwanth",
+            "age": 24,
+            "number": 983748378,
+            "marks": 60
+        }
+    ]
+
+    return render(request, 'home.html', {'students': students})
