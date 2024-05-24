8d0046ecafda9872c753717d97ae6db431f2d2aa
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-05-24 12:09:59 +0530

    Extending User Model to store more details

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile = models.BigIntegerField()
+    gender = models.CharField(max_length=10)
+    dob = models.DateField()
--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from . models import UserDetails
 # Create your views here.
 
 
@@ -10,24 +11,37 @@ def register(request):
         lastName = request.POST['lastName']
         username = request.POST['username']
         password = request.POST['password']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        gender = request.POST['gender']
+        mobile = request.POST['mobile']
 
-        User.objects.create_user(
+        user = User.objects.create_user(
             first_name=firstName,
             last_name=lastName,
             username=username,
             password=password,
+            email=email
+        )
+
+        UserDetails.objects.create(
+            gender=gender,
+            dob=dob,
+            mobile=mobile,
+            user_id=user.id
         )
 
     return render(request, 'accounts/register.html')
+
 
 def loginUser(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
+
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('homepage')
-    
+
     return render(request, 'accounts/login.html')
