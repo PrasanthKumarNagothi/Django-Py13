d47b5e74f93f169b810ce993c19a0c83f22a1563
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-05-25 11:58:37 +0530

    Django Media Files

--- instagram/feed/models.py
+++ instagram/feed/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='images')
+    date = models.DateTimeField(auto_now_add=True)
+    caption = models.TextField()
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
