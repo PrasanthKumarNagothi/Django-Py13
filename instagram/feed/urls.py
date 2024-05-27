b5581fdf5a89de835ab436e21115e23221acaac9
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-05-27 12:32:48 +0530

    File Handling

--- instagram/feed/urls.py
+++ instagram/feed/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.homepage, name='homefeed'),
+    path('upload', views.upload, name='upload'),
 ]
--- instagram/feed/views.py
+++ instagram/feed/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse('Feed app homepage')
+    posts = Post.objects.all()
+    return render(request, 'feed/home.html', {"posts" : posts})
+
+@login_required(login_url='login')
+def upload(request):
+    if request.method == 'POST':
+        image = request.FILES['image']
+        caption = request.POST['caption']
+        Post.objects.create(
+            image=image,
+            caption=caption,
+            userid_id = request.user.id
+        )
+        return redirect('upload')
+    return render(request, 'feed/upload.html')
